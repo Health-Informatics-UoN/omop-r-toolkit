a794ac8552ffac0cc9c85d8f4264846b557cd9e3
James Mitchell-White <jamesjimitchell@gmail.com>
2026-07-08 20:28:14 +0100

    Update count-cohorts.R

--- R/count-cohorts.R
+++ R/count-cohorts.R
@@ -7,7 +7,7 @@ Options:
   -h --help                     Show this screen
   --version                     Show version
   --alloccurrences              Include all occurrences of events in the cohort. Otherwise, only includes the first
-  --end=<end>                   How the cohort end date should be defined. One of "observation_end_date", a numeric scalar for the number of days, or "event_end_date" [default: observation_end_date]
+  --end=<end>                   How the cohort end date should be defined. One of "observation_period_end_date", a numeric scalar for the number of days, or "event_end_date" [default: observation_period_end_date]
   --requiredObservation=<days>  Comma-separated pair of days of required observation time prior,post index, e.g. "0,0" [default: 0,0]
   --conceptSet=<json>           JSON string describing the concept set for the cohort ({"someName": [1234, 5678],...})
 
