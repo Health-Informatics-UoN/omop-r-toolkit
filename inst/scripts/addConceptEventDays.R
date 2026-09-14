be820a9468566c35b2626f9aa22cb3cb1d70d6b5
CodeByKarthik <karthinottsuk@gmail.com>
2026-09-14 12:46:00 +0100

    Updated CDM Connection

--- inst/scripts/addConceptEventDays.R
+++ inst/scripts/addConceptEventDays.R
@@ -28,14 +28,14 @@ source("R/parseMultipleEvents.R")
 
 arguments <- docopt(doc, version = "Add Concept Event Days 0.1.0")
 
-cdm <- connectFiveSafesTESPg("postgres_omop", cohortTables = arguments$name)
-cohort <- cdm[[arguments$name]]
-orig_names <- colnames(cohort)
-
 conceptSet <- parseJSONConceptSet(arguments$conceptSet)
 window <- parseWindows(arguments$window)
 censorDate <- if (!is.null(arguments$censorDate)) arguments$censorDate else NULL
 multipleEvents <- parseMultipleEvents(arguments$multipleEvents)
+
+cdm <- connectFiveSafesTESPg("postgres_omop", cohortTables = arguments$name)
+cohort <- cdm[[arguments$name]]
+orig_names <- colnames(cohort)
 
 cohort <- cohort |>
   addConceptEventDays(
